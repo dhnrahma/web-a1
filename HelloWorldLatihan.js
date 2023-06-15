f4509e9eeba7af9b6b974fe44d694e2d3b529aae
dhnrahma <125951596+dhnrahma@users.noreply.github.com>
2023-06-15 10:17:09 +0700

    Add files via upload

---
+++ HelloWorldLatihan.js
@@ -0,0 +1,20 @@
+document.writeln("Hello World");
+//Perkenalan
+document.writeln("<p> Rahmadhani Utami </p>");
+document.writeln("</br>");
+document.writeln("<p> NIM 20105244005 </p>");
+document.writeln("<p> Kelas : A1 </p>");
+document.writeln(0b10101);
+document.writeln("</textarea");
+document.writeln("</br")
+
+var fullName = "Rahmadhani Utami";
+var firstName = "Rahmadhani";
+var lastName = "Utami";
+
+document.writeln("Nama Lengkap : "+ fullName);
+document.writeln("</br>");
+document.writeln("Nama Lengkap :"+ firstName);
+document.writeln("</br>")
+document.writeln("Nama Lengkap :"+ lastName);
+
